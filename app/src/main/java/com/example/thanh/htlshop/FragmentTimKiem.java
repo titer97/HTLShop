950dfb164fe56a7da2d0b735f1dde20c5b2107f9
titer97 <thanhnguyentsbd@gmail.com>
2018-12-06 18:31:07 +0700

    thanh đã thêm các fragment vào

---
+++ app/src/main/java/com/example/thanh/htlshop/FragmentTimKiem.java
@@ -0,0 +1,18 @@
+package com.example.thanh.htlshop;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTimKiem extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tim_kiem,container,false);
+        return view;
+    }
+}
