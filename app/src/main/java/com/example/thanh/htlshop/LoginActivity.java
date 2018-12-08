37bbf76732ed32785d9b900bf63b2a8ac7555dc9
titer97 <thanhnguyentsbd@gmail.com>
2018-12-08 12:59:14 +0700

    Thanh thêm chức năng đăng nhập

---
+++ app/src/main/java/com/example/thanh/htlshop/LoginActivity.java
@@ -0,0 +1,335 @@
+package com.example.thanh.htlshop;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.thanh.model.TaiKhoan;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private UserLoginTask mAuthTask = null;
+
+    private AutoCompleteTextView mUsernameView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+    private String FileName = "UsernameAndPassword";
+    private String usernameHienTai;
+    private String passwordHienTai;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mUsernameView = findViewById(R.id.username);
+
+        mPasswordView = findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mSignInButton = findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new UserLoginTask(username, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+/*    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 2;
+    }*/
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+   /* public class UserLoginTask extends AsyncTask<Void,Void,ArrayList<TaiKhoan>>{
+
+        private final String mUsername;
+        private final String mPassword;
+
+        UserLoginTask(String username, String password) {
+            mUsername = username;
+            mPassword = password;
+        }
+
+        @Override
+        protected ArrayList<TaiKhoan> doInBackground(Void... voids) {
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            }
+            ArrayList<TaiKhoan> listTaiKhoan = new ArrayList<>();
+            try {
+                URL url = new URL("http://tripletstore.somee.com/api/taikhoan");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                InputStreamReader isr = new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                StringBuilder builder = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    builder.append(line);
+                }
+                JSONArray jsonArray = new JSONArray(builder.toString());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    TaiKhoan tk = new TaiKhoan();
+                    tk.setMakh(jsonObject.getInt("makh"));
+                    tk.setUsername(jsonObject.getString("username"));
+                    tk.setPassword(jsonObject.getString("password"));
+                }
+                br.close();
+                isr.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return listTaiKhoan;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            mAuthTask = null;
+            showProgress(false);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<TaiKhoan> taiKhoans) {
+            super.onPostExecute(taiKhoans);
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                moMainActivity(mUsername,mPassword);
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+    }*/
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mUsername;
+        private final String mPassword;
+
+        UserLoginTask(String username, String password) {
+            mUsername = username;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+            ArrayList<TaiKhoan> listTaiKhoan = new ArrayList<>();
+            try {
+                URL url = new URL("http://tripletstore.somee.com/api/taikhoan");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                InputStreamReader isr = new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                StringBuilder builder = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    builder.append(line);
+                }
+                JSONArray jsonArray = new JSONArray(builder.toString());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    TaiKhoan tk = new TaiKhoan();
+                    tk.setMakh(jsonObject.getInt("makh"));
+                    tk.setUsername(jsonObject.getString("username"));
+                    tk.setPassword(jsonObject.getString("password"));
+                    listTaiKhoan.add(tk);
+                }
+                br.close();
+                isr.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            for (TaiKhoan tk2 : listTaiKhoan) {
+                String username = tk2.getUsername();
+                String password = tk2.getPassword();
+                if (username.equals(mUsername) && password.equals(mPassword)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                moMainActivity(mUsername, mPassword);
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+
+    private void moMainActivity(String username, String password) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("username", username);
+        returnIntent.putExtra("password", password);
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
+    }
+}
+
--- app/src/main/java/com/example/thanh/htlshop/MainActivity.java
+++ app/src/main/java/com/example/thanh/htlshop/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.thanh.htlshop;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
@@ -14,19 +21,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    FragmentManager fragmentManager;
+    private FragmentManager fragmentManager;
+    private MenuItem itemDangNhap;
+    private String usernameHienTai;
+    private String passwordHienTai;
+    private TextView txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        String a="123";
+        String a = "123";
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,25 +54,25 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView =  findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Menu menuNav = navigationView.getMenu();
+        itemDangNhap = menuNav.findItem(R.id.nav_dang_nhap);
+
+        docUsernamePassword();
+
+        View header_view = navigationView.getHeaderView(0);
+        txtEmail = header_view.findViewById(R.id.txtEmail);
+        txtEmail.setText(usernameHienTai);
 
         fragmentManager = getSupportFragmentManager();
         FragmentPhuKienXeMay fragmentPhuKienXeMay = new FragmentPhuKienXeMay();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.main_context,fragmentPhuKienXeMay);
+        transaction.add(R.id.main_context, fragmentPhuKienXeMay);
         transaction.commit();
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+    private String FileName = "UsernameAndPassword";
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -90,29 +102,97 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-         if (id == R.id.nav_phu_kien_xe_may) {
-             FragmentPhuKienXeMay fragmentPhuKienXeMay = new FragmentPhuKienXeMay();
-             FragmentTransaction transaction = fragmentManager.beginTransaction();
-             transaction.replace(R.id.main_context,fragmentPhuKienXeMay);
-             transaction.commit();
+        if (id == R.id.nav_phu_kien_xe_may) {
+            FragmentPhuKienXeMay fragmentPhuKienXeMay = new FragmentPhuKienXeMay();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_context, fragmentPhuKienXeMay);
+            transaction.commit();
         } else if (id == R.id.nav_gio_hang) {
-             FragmentHoaDon fragmentHoaDon = new FragmentHoaDon();
-             FragmentTransaction transaction = fragmentManager.beginTransaction();
-             transaction.replace(R.id.main_context,fragmentHoaDon);
-             transaction.commit();
+            FragmentHoaDon fragmentHoaDon = new FragmentHoaDon();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_context, fragmentHoaDon);
+            transaction.commit();
         } else if (id == R.id.nav_tim_kiem) {
-             FragmentTimKiem fragmentTimKiem = new FragmentTimKiem();
-             FragmentTransaction transaction = fragmentManager.beginTransaction();
-             transaction.replace(R.id.main_context,fragmentTimKiem);
-             transaction.commit();
+            FragmentTimKiem fragmentTimKiem = new FragmentTimKiem();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_context, fragmentTimKiem);
+            transaction.commit();
         } else if (id == R.id.nav_thong_tin_tai_khoan) {
 
         } else if (id == R.id.nav_dang_nhap) {
+            if (itemDangNhap.getTitle().equals("Đăng nhập")) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivityForResult(intent, 1);
+            } else if (itemDangNhap.getTitle().equals("Đăng xuất")) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Đăng xuất");
+                builder.setMessage("Bạn có thật sự muốn đăng xuất?");
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        SharedPreferences sharedPreferences = getSharedPreferences(FileName, Context.MODE_PRIVATE);
+                        sharedPreferences.edit().clear().apply();
+                        itemDangNhap.setTitle("Đăng nhập");
+                        txtEmail.setText("abc@gmail.com");
+                    }
+                }).setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
+                    }
+                }).show();
+            }
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                String mUsername = data.getStringExtra("username");
+                String mPassword = data.getStringExtra("password");
+                luuUsernamePassword(mUsername, mPassword);
+                itemDangNhap.setTitle("Đăng xuất");
+                txtEmail.setText(mUsername);
+            }
+        }
+    }
+
+    private void luuUsernamePassword(String mUsername, String mPassword) {
+        SharedPreferences sharedPreferences = getSharedPreferences(FileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", mUsername);
+        editor.putString("password", mPassword);
+        editor.apply();
+    }
+
+    private void docUsernamePassword() {
+        SharedPreferences sharedPreferences = getSharedPreferences(FileName, Context.MODE_PRIVATE);
+        String defaultValue = "abc@gmail.com";
+        usernameHienTai = sharedPreferences.getString("username", defaultValue);
+        String defaultValue2 = "password";
+        passwordHienTai = sharedPreferences.getString("password", defaultValue2);
+        if (usernameHienTai.equals("abc@gmail.com")) {
+            //nếu ko có dữ liệu
+            itemDangNhap.setTitle("Đăng nhập");
+        } else {
+            //nếu có lưu dữ liệu
+            itemDangNhap.setTitle("Đăng xuất");
+        }
+    }
+
+   /* @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }*/
 }
