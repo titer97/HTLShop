15d5b780976935dd55cf137acd49e3dc242374c0
Tien <minhtienbrom@gmail.com>
2018-12-12 15:24:47 +0700

    Minh Tien - dang ky tai khoan.

--- app/src/main/java/com/example/thanh/htlshop/LoginActivity.java
+++ app/src/main/java/com/example/thanh/htlshop/LoginActivity.java
@@ -33,6 +33,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.thanh.model.TaiKhoan;
 
@@ -51,7 +52,6 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity {
 
     private UserLoginTask mAuthTask = null;
-
     private AutoCompleteTextView mUsernameView;
     private EditText mPasswordView;
     private View mProgressView;
@@ -59,6 +59,7 @@ public class LoginActivity extends AppCompatActivity {
     private String FileName = "UsernameAndPassword";
     private String usernameHienTai;
     private String passwordHienTai;
+    private Button btnDangKy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +73,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    attemptLogin(1);
                     return true;
                 }
                 return false;
@@ -83,15 +84,23 @@ public class LoginActivity extends AppCompatActivity {
         mSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                attemptLogin(1);
             }
         });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        btnDangKy = findViewById(R.id.btnDangKy);
+        btnDangKy.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin(2);
+            }
+        });
     }
 
-    private void attemptLogin() {
+    private void attemptLogin(int type) {
         if (mAuthTask != null) {
             return;
         }
@@ -127,9 +136,17 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(username, password);
-            mAuthTask.execute((Void) null);
+            if (type == 1) {
+                //dang nhap
+                showProgress(true);
+                mAuthTask = new UserLoginTask(username, password);
+                mAuthTask.execute((Void) null);
+            } else if (type == 2) {
+                //dang ky
+                showProgress(true);
+                DangKyTask task = new DangKyTask(username, password);
+                task.execute((Void) null);
+            }
         }
     }
 
@@ -174,78 +191,6 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-   /* public class UserLoginTask extends AsyncTask<Void,Void,ArrayList<TaiKhoan>>{
-
-        private final String mUsername;
-        private final String mPassword;
-
-        UserLoginTask(String username, String password) {
-            mUsername = username;
-            mPassword = password;
-        }
-
-        @Override
-        protected ArrayList<TaiKhoan> doInBackground(Void... voids) {
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-            }
-            ArrayList<TaiKhoan> listTaiKhoan = new ArrayList<>();
-            try {
-                URL url = new URL("http://tripletstore.somee.com/api/taikhoan");
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("GET");
-                httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                InputStreamReader isr = new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8");
-                BufferedReader br = new BufferedReader(isr);
-                StringBuilder builder = new StringBuilder();
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    builder.append(line);
-                }
-                JSONArray jsonArray = new JSONArray(builder.toString());
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    TaiKhoan tk = new TaiKhoan();
-                    tk.setMakh(jsonObject.getInt("makh"));
-                    tk.setUsername(jsonObject.getString("username"));
-                    tk.setPassword(jsonObject.getString("password"));
-                }
-                br.close();
-                isr.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return listTaiKhoan;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onCancelled() {
-            super.onCancelled();
-            mAuthTask = null;
-            showProgress(false);
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<TaiKhoan> taiKhoans) {
-            super.onPostExecute(taiKhoans);
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                moMainActivity(mUsername,mPassword);
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-    }*/
 
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
@@ -253,7 +198,7 @@ public class LoginActivity extends AppCompatActivity {
         private final String mPassword;
         private int mMaKh;
 
-        UserLoginTask(String username, String password ){
+        UserLoginTask(String username, String password) {
             mUsername = username;
             mPassword = password;
         }
@@ -296,12 +241,12 @@ public class LoginActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             for (TaiKhoan tk2 : listTaiKhoan) {
-                    String username = tk2.getUsername();
-                    String password = tk2.getPassword();
-                    mMaKh = tk2.getMakh();
-                    if (username.equals(mUsername) && password.equals(mPassword)) {
-                        return true;
-                    }
+                String username = tk2.getUsername();
+                String password = tk2.getPassword();
+                mMaKh = tk2.getMakh();
+                if (username.equals(mUsername) && password.equals(mPassword)) {
+                    return true;
+                }
             }
             return false;
         }
@@ -312,7 +257,7 @@ public class LoginActivity extends AppCompatActivity {
             showProgress(false);
 
             if (success) {
-                moMainActivity(mUsername, mPassword,mMaKh);
+                moMainActivity(mUsername, mPassword, mMaKh);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
@@ -326,13 +271,116 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void moMainActivity(String username, String password,int makh) {
+    private void moMainActivity(String username, String password, int makh) {
         Intent returnIntent = new Intent();
         returnIntent.putExtra("username", username);
         returnIntent.putExtra("password", password);
-        returnIntent.putExtra("makh",makh);
+        returnIntent.putExtra("makh", makh);
         setResult(Activity.RESULT_OK, returnIntent);
         finish();
     }
+
+    public class DangKyTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mUsername;
+        private final String mPassword;
+
+        DangKyTask(String username, String password) {
+            mUsername = username;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            try {
+                //lay danh sach tai khoan
+                ArrayList<TaiKhoan> listTaiKhoan = new ArrayList<>();
+                URL url = new URL("http://tripletstore.somee.com/api/taikhoan");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                InputStreamReader isr = new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                StringBuilder builder = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    builder.append(line);
+                }
+                JSONArray jsonArray = new JSONArray(builder.toString());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    TaiKhoan tk = new TaiKhoan();
+                    tk.setMakh(jsonObject.getInt("makh"));
+                    tk.setUsername(jsonObject.getString("username"));
+                    tk.setPassword(jsonObject.getString("password"));
+                    //them tai khoan vao danh sach
+                    listTaiKhoan.add(tk);
+                }
+                //kiem tra trong danh sach neu ton tai username thi khong cho dang ky
+                for (TaiKhoan tk2 : listTaiKhoan) {
+                    String username = tk2.getUsername();
+                    if (username.equals(mUsername)) {
+                        return false;
+                    } else {
+                        //dang ky tai khoan
+                        String thamSo = "username=" + mUsername + "&password=" + mPassword;
+                        URL url2 = new URL("http://tripletstore.somee.com/api/taikhoan/?" + thamSo);
+                        HttpURLConnection httpURLConnection2 = (HttpURLConnection) url2.openConnection();
+                        httpURLConnection.setRequestMethod("POST");
+                        httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                        InputStreamReader isr2 = new InputStreamReader(httpURLConnection2.getInputStream(), "UTF-8");
+                        BufferedReader br2 = new BufferedReader(isr2);
+                        StringBuilder builder2 = new StringBuilder();
+                        String line2 = null;
+                        while ((line2 = br2.readLine()) != null) {
+                            builder2.append(line);
+                        }
+                        //neu ket qua tra ve True thi dang ky tai khoan thanh cong
+                        boolean ketqua = builder2.toString().contains("true");
+                        br.close();
+                        isr.close();
+                        return ketqua;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                Toast.makeText(getApplication(), "Đăng ký tài khoản thành công!", Toast.LENGTH_LONG).show();
+                mUsernameView.setText(mUsername);
+                mPasswordView.setText("");
+                mPasswordView.requestFocus();
+            } else {
+                Toast.makeText(getApplication(), "Đăng ký tài khoản thất bại!", Toast.LENGTH_LONG).show();
+                mUsernameView.setText("");
+                mPasswordView.setText("");
+                mUsernameView.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+
 }
 
